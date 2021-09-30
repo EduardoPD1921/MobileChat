3a31d10383e4181792cdc672a0c83763fe380c32
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-09-30 20:17:49 -0300

    :sparkles: Sign up snackbar finished

--- src/screens/Signup/index.js
+++ src/screens/Signup/index.js
@@ -3,15 +3,28 @@ import { View, Animated, Text, Button } from 'react-native';
 
 import { containerStyle } from './styles';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SignupForm from '../../components/Forms/SignupForm';
 
 function Register() {
-  // const window = Dimensions.get('window');
   const verticalAnim = useRef(new Animated.Value(0)).current;
 
   function verticalUp() {
     Animated.timing(verticalAnim, {
-      toValue: -20,
+      toValue: -50,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+
+    setTimeout(() => {
+      verticalDown();
+    }, 4000);
+  };
+
+  function verticalDown() {
+    Animated.timing(verticalAnim, {
+      toValue: 0,
       duration: 200,
       useNativeDriver: true
     }).start();
@@ -19,9 +32,30 @@ function Register() {
 
   return (
     <View style={containerStyle.mainContainer}>
-      <SignupForm />
-      {/* <Button title="teste" onPress={verticalUp} />
-      <Animated.View style={{ position: 'absolute', backgroundColor: 'blue', width: '100%', height: 20, transform: [{ translateY: verticalAnim }], bottom: -20 }}>
+      {/* <SignupForm /> */}
+      <Button title="teste" onPress={verticalUp} />
+      <Animated.View style={{ position: 'absolute', width: '100%', transform: [{ translateY: verticalAnim }], bottom: -40, alignItems: 'center' }}>
+        <View style={{ backgroundColor: '#3BBD9F', width: '90%', height: 40, borderRadius: 10, flexDirection: 'row', alignItems: 'center' }}>
+          <Icon
+            name="verified"
+            color="#B7E4C7"
+            size={25}
+            style={{marginLeft: 20, marginRight: 10 }}
+          />
+          <Text style={{ color: '#B7E4C7', fontFamily: 'Poppins-Regular' }}>Conta criada com sucesso!</Text>
+          <Icon
+            onPress={() => verticalDown()}
+            name="close"
+            size={15}
+            color="#B7E4C7" 
+            style={{ marginLeft: '20%' }}
+          />
+        </View>
+      </Animated.View>
+      {/* <Animated.View style={{ position: 'absolute', backgroundColor: '#2B354F', width: '100%', height: 40, transform: [{ translateY: verticalAnim }], bottom: -40, borderTopLeftRadius: 5, borderTopRightRadius: 5 }}>
+        <Icon
+          name="verified" 
+        />
         <Text>Teste</Text>
       </Animated.View> */}
     </View>
