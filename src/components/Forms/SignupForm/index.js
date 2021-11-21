2c9360d17d9d05316e2d88478993ae83e761c8e5
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-11-21 16:52:26 -0300

    :construction: Implementing form inputs with formik

--- src/components/Forms/SignupForm/index.js
+++ src/components/Forms/SignupForm/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Text, View, ScrollView, TextInput } from 'react-native';
 
+import { useFormik } from 'formik';
+
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 import { signupFormStyles } from './styles';
 
 function SignupForm() {
+  const { handleChange, handleSubmit } = useFormik({
+    initialValues: { userName: '', userEmail: '', userPhone: '', userPassword: '' },
+    onSubmit: values => {
+      console.log(values);
+    }
+  });
+
   return (
     <ScrollView style={signupFormStyles.formContainer}>
-      <View style={{ marginTop: 20 }}>
-        {/* <Text style={{ color : '#b3b3b3' }}>Nome</Text> */}
-        <TextInput placeholder="Nome" style={{ borderBottomWidth: 1, width: '80%', borderColor: '#52B788' }} />
+      <View>
+        <View style={signupFormStyles.inputContainer}>
+          <FeatherIcon name="user" color="#52B788" size={20} style={signupFormStyles.icon} />
+          <TextInput onChangeText={handleChange('userName')} placeholder="Nome" style={signupFormStyles.input} />
+        </View>
+        <View style={[signupFormStyles.inputContainer, { marginTop: 30 }]}>
+          <FeatherIcon name="mail" color="#52B788" size={20} style={signupFormStyles.icon} />
+          <TextInput onChangeText={handleChange('userEmail')} placeholder="E-mail" style={signupFormStyles.input} />
+        </View>
+        <View style={[signupFormStyles.inputContainer, { marginTop: 30 }]}>
+          <FeatherIcon name="smartphone" color="#52B788" size={20} style={signupFormStyles.icon} />
+          <TextInput onChangeText={handleChange('userPhone')} placeholder="Telefone" style={signupFormStyles.input} />
+        </View>
+        <View style={[signupFormStyles.inputContainer, { marginTop: 30 }]}>
+          <FeatherIcon name="lock" color="#52B788" size={20} style={signupFormStyles.icon} />
+          <TextInput onChangeText={handleChange('userPassword')} secureTextEntry placeholder="Senha" style={[signupFormStyles.input, { paddingRight: 20 }]} />
+        </View>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Text onPress={handleSubmit}>Teste</Text>
+        </View>
       </View>
     </ScrollView>
   );
--- src/components/Forms/SignupForm/styles.js
+++ src/components/Forms/SignupForm/styles.js
@@ -3,5 +3,18 @@ import { StyleSheet } from 'react-native';
 export const signupFormStyles = StyleSheet.create({
   formContainer: {
     marginLeft: 20
+  },
+  inputContainer: {
+    justifyContent: 'center',
+  },
+  icon: {
+    position: 'absolute',
+    left: 10
+  },
+  input: {
+    borderBottomWidth: 1,
+    width: '90%',
+    borderColor: '#52B788',
+    paddingLeft: 40
   }
 });
